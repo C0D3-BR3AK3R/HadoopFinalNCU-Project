f251ddab139718bedba06c3cb97e55c92e3dd688
Dhruvarshi Das <dasdhruvarshi@gmail.com>
2022-07-19 13:51:46 +0530

    Created Flask Webapp and setup comment stream

--- YT-WebApp/FetchingComments.py
+++ YT-WebApp/FetchingComments.py
@@ -1,4 +1,56 @@
 import socket
-
+from googleapiclient.discovery import build
 
 API_key = 'AIzaSyBlQUfsYPB9PqrlTUk-9HlEZOUiRg53LqQ'
+video_ID = "5-eFLcCDNo8"
+
+class FetchComments():
+    
+    def __init__(self, api_key, vid_id):
+
+        resource = build('youtube', 'v3', developerKey=api_key)
+
+        request = resource.commentThreads().list(part="snippet", videoId=vid_id, maxResults= 50, order="orderUnspecified")
+        response = request.execute()
+
+        items = response['items'][:]
+
+        print("------------------------------------------------------------------------------------------------------")
+        for item in items:
+            item_info = item["snippet"]
+            
+            #the top level comment can have sub reply comments
+            topLevelComment = item_info["topLevelComment"]
+            comment_info = topLevelComment["snippet"]
+            
+            try:
+                comment_text = comment_info['textDisplay'].encode('utf-8')
+                print("Coment Text:" ,comment_text)
+                clientSocket.send((comment_text+'\n'))
+            except:
+                print('Issue in fetching comment.')
+            
+            
+            """ print("Comment By:", comment_info["authorDisplayName"].encode('utf-8'))
+            print("Coment Text:" ,comment_info["textDisplay"].encode('utf-8'))
+            print("Likes on Comment :", comment_info["likeCount"])
+            print("Comment Date: ", comment_info['publishedAt'])
+            print("================================\n") """
+            
+            
+serverSocket = socket.socket()
+HOST = "127.0.0.1"
+PORT = 9999
+serverSocket.bind((HOST, PORT))
+print("Server is ready...Waiting for Client...")
+serverSocket.listen(4)
+
+clientSocket, address = serverSocket.accept()   # Handshaking
+print("Client Arrived...")
+
+obj = FetchComments(api_key=API_key, vid_id=video_ID)
+
+
+
+
+    
---
+++ YT-WebApp/server.py
@@ -0,0 +1,81 @@
+import os, sys
+from tracemalloc import start
+from pyspark.sql import SparkSession
+import threading
+import pandas as pd
+from datetime import datetime as dt
+import glob
+import time
+from flask import Flask
+from flask import render_template
+from turbo_flask import Turbo
+import random
+
+app  = Flask(__name__)
+turbo = Turbo(app)
+
+def start_streaming():
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+    
+    spark = SparkSession.builder.appName('YT Comment Streamer').getOrCreate()
+    HOST = 'localhost'
+    PORT = 9999
+    
+    # Reading data from network
+    
+    lines = (spark
+             .readStream
+             .format("socket")
+             .option("host", HOST)
+             .option("port", PORT)
+             .load())
+    
+    print("Is Streaming :",lines.isStreaming)
+
+    # lines.writeStream.format("console").option("truncate",False).start()
+    (lines
+    .writeStream
+    .format("csv")
+    .option("checkpointLocation", "checkpoint/")
+    .option("path", "output_dir/")
+    .option("truncate",False)
+    .outputMode("append")
+    .start())
+
+    spark.streams.awaitAnyTermination()
+    spark.close()
+
+def update_load():
+    with app.app_context():
+        while True:
+            try:
+                time.sleep(2)
+                t = dt.now().time()
+                print("Current Time is :",t)
+                
+                csv_files = glob.glob("output_dir/*.csv")
+                print("Number of Files : ", len(csv_files))
+
+                file = random.choice(csv_files)
+                df = pd.read_csv(file, header=None)
+                print("Data Inside CSV",df)
+
+                tweet_text = df.values[0][0]
+
+                turbo.push(turbo.replace(render_template('load_tweets.html', current_time=t, tweet_text=tweet_text), 'load'))
+
+            except BaseException as ex:
+                pass
+            
+@app.before_first_request
+def before_first_request():
+    threading.Thread(target=update_load).start()
+
+@app.route('/')
+def index():
+    threading.Thread(target=start_streaming).start()
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
